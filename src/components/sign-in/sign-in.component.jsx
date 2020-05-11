a2c20ebc975a9fd4f3d83be6e53d132bcfa92c6b
miraDask <mira.daskalov@gmail.com>
2020-05-11 14:29:48 +0100

    add custom-button component and add firebase to

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
+import CustomButton from '../custom-button/custom-button.component'
 
 import "./sign-in.styles.scss";
 
@@ -42,7 +43,7 @@ class SignIn extends React.Component {
             required
             handleOnChange={this.handleOnChange}
           />
-          <input type="submit" value="Submit"></input>
+          <CustomButton type="submit"> Sign in </CustomButton>
         </form>
       </div>
     );
