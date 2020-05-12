f97700e59f7298627654872a4de82a617075f130
miraDask <mira.daskalov@gmail.com>
2020-05-12 15:50:16 +0100

    implement sign in with firebase

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import React from "react";
-import HomePage from "./pages/homepage/homepage.component";
-import { Switch, Route } from "react-router-dom";
-import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "../src/firebase/firebase.utils";
+import React from 'react';
+import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth, createUserProfileDocument } from '../src/firebase/firebase.utils';
 
-import "./App.css";
-import "./pages/homepage/homepage.styles.scss";
+import './App.css';
+import './pages/homepage/homepage.styles.scss';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null,
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			currentUser: null
+		};
+	}
 
-  unsubscribeFromAuth = null;
+	unsubscribeFromAuth = null;
 
-  componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-    });
-  }
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+				userRef.onSnapshot((snapShot) => {
+					this.setState({ currentUser });
+				});
+			}
+		});
+	}
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
 
-  render() {
-    return (
-      <div>
-        <Header currentUser={this.state.currentUser} />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={SignInAndSignUpPage} />
-        </Switch>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Header currentUser={this.state.currentUser} />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/shop" component={ShopPage} />
+					<Route exact path="/signin" component={SignInAndSignUpPage} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,28 +1,54 @@
 import firebase from 'firebase/app';
 
-
 import 'firebase/firestore';
 import 'firebase/auth';
 
 const configuration = {
-    apiKey: "AIzaSyCidFQfo1w5hNs_XIYEBPkZ6t1_0NYn4DI",
-    authDomain: "confidence-shop.firebaseapp.com",
-    databaseURL: "https://confidence-shop.firebaseio.com",
-    projectId: "confidence-shop",
-    storageBucket: "confidence-shop.appspot.com",
-    messagingSenderId: "601037202062",
-    appId: "1:601037202062:web:a5c397bb3c40750a66dfdf",
-    measurementId: "G-CK7QZFBQRM"
-  };
+	apiKey: 'AIzaSyCidFQfo1w5hNs_XIYEBPkZ6t1_0NYn4DI',
+	authDomain: 'confidence-shop.firebaseapp.com',
+	databaseURL: 'https://confidence-shop.firebaseio.com',
+	projectId: 'confidence-shop',
+	storageBucket: 'confidence-shop.appspot.com',
+	messagingSenderId: '601037202062',
+	appId: '1:601037202062:web:a5c397bb3c40750a66dfdf',
+	measurementId: 'G-CK7QZFBQRM'
+};
 
-  firebase.initializeApp(configuration); 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) {
+		return;
+	}
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt : 'select_account'});
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdOn = new Date();
 
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdOn,
+				...additionalData
+			});
+		} catch (error) {
+			console.log('error creating user', error.message);
+		}
+	}
 
-  export default firebase;
+	return userRef;
+};
+
+firebase.initializeApp(configuration);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
