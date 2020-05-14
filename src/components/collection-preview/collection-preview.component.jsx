4ea4c5858e9eb061728cdb642956b2a5bb63d280
miraDask <mira.daskalov@gmail.com>
2020-05-14 17:47:13 +0100

    update shop-data, add cart.utils to help group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,19 +1,15 @@
-import React from "react";
-import CollectionItem from '../collection-item/collection-item.component'
+import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
 
-import "./collection-preview.styles.scss";
+import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items, id }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
-      {items
-        .filter((item, indx) => indx < 4)
-        .map(({id, ...otherCollectionItemProps}) => (
-          <CollectionItem key={id} {...otherCollectionItemProps}/>
-        ))}
-    </div>
-  </div>
+const CollectionPreview = ({ title, items }) => (
+	<div className="collection-preview">
+		<h1 className="title">{title.toUpperCase()}</h1>
+		<div className="preview">
+			{items.filter((item, indx) => indx < 4).map((item) => <CollectionItem key={item.id} item={item} />)}
+		</div>
+	</div>
 );
 
 export default CollectionPreview;
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import "./custom-button.styles.scss";
+import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignInButton, ...otherProps }) => (
-  <button className={`custom-button ${isGoogleSignInButton ? 'google-sign-in' : ''}`} {...otherProps}>
-    {children}
-  </button>
+const CustomButton = ({ children, isGoogleSignInButton, inverted, ...otherProps }) => (
+	<button
+		className={`custom-button ${inverted ? 'inverted' : ''} ${isGoogleSignInButton ? 'google-sign-in' : ''}`}
+		{...otherProps}
+	>
+		{children}
+	</button>
 );
 
 export default CustomButton;
