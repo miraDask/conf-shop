d099cf06d653a5ad6c34c9255ff0cb29f92a6e7e
miraDask <mira.daskalov@gmail.com>
2020-05-14 14:25:11 +0100

    implement urerReducer and userActions in app and header components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,33 +1,39 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
 
-import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
-import { auth } from "../../firebase/firebase.utils";
-
-import "./header.styles.scss";
+import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
-  <div className="header">
-    <Link to="/" className="logo-container">
-      <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link to="/shop" className="option">
-        SHOP
-      </Link>
-      <Link to="/contact" className="option">
-        CONTACT
-      </Link>
-      {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-      ) : (
-        <Link to="/signin" className="option">
-          {" "}
-          SIGN IN
-        </Link>
-      )}
-    </div>
-  </div>
+	<div className="header">
+		<Link to="/" className="logo-container">
+			<Logo className="logo" />
+		</Link>
+		<div className="options">
+			<Link to="/shop" className="option">
+				SHOP
+			</Link>
+			<Link to="/contact" className="option">
+				CONTACT
+			</Link>
+			{currentUser ? (
+				<div className="option" onClick={() => auth.signOut()}>
+					SIGN OUT
+				</div>
+			) : (
+				<Link to="/signin" className="option">
+					{' '}
+					SIGN IN
+				</Link>
+			)}
+		</div>
+	</div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+	currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
