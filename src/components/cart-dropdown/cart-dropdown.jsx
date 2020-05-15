7ae8bf5f7d60f368dcc12ef388f4a33f92c99c9d
miraDask <mira.daskalov@gmail.com>
2020-05-15 15:28:04 +0100

    implement memoized cart selectors, add reselect

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import CartItem from '../cart-item/cart.item.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -13,8 +14,8 @@ const CartDropdown = ({ cartItems }) => (
 	</div>
 );
 
-const mapStageToProps = ({ cart: { cartItems } }) => ({
-	cartItems
+const mapStageToProps = (state) => ({
+	cartItems: selectCartItems(state)
 });
 
 export default connect(mapStageToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
@@ -13,8 +13,8 @@ const CartIcon = ({ toggleCartHidden, itemsCount }) => (
 	</div>
 );
 
-const mapStageToProps = ({ cart: { cartItems } }) => ({
-	itemsCount: cartItems.reduce((acc, curr) => acc + curr.quantity, 0)
+const mapStageToProps = (state) => ({
+	itemsCount: selectCartItemsCount(state)
 });
 
 const mapDispatchToProps = (dispatch) => ({
