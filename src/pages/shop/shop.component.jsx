037cf80aa09adb229cfcae992cd1d63a978fc5d6
miraDask <mira.daskalov@gmail.com>
2020-05-16 18:23:39 +0100

    add collection-overview component and implemnt dinamicaly routing for collection page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { Route } from 'react-router-dom';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
 	<div className="shop-page">
-		{collections.map(({ id, ...otherCollectionProps }) => <CollectionPreview key={id} {...otherCollectionProps} />)}
+		<Route exact path={`${match.path}`} component={CollectionsOverview} />
+		<Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
 	</div>
 );
 
-const matStateToProps = createStructuredSelector({ collections: selectCollections });
-
-export default connect(matStateToProps)(ShopPage);
+export default ShopPage;
