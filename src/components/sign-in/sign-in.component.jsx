6e284ca7246adfe541c059a745cdd96fc5de8670
miraDask <mira.daskalov@gmail.com>
2020-05-17 19:10:13 +0100

    replace css files with styled-components for sign up and sign-in components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.styles.scss';
+import { SignInContainer, TitleContainer, ButtonsContainer } from './sign-in.styles';
 
 class SignIn extends React.Component {
 	constructor(props) {
@@ -34,8 +34,8 @@ class SignIn extends React.Component {
 	render() {
 		const { email, password } = this.state;
 		return (
-			<div className="sign-in">
-				<h2 className="title">You already have an account ?</h2>
+			<SignInContainer>
+				<TitleContainer>You already have an account ?</TitleContainer>
 				<span>Sign in with your email and password</span>
 				<form onSubmit={this.handleSubmit}>
 					<FormInput
@@ -54,15 +54,15 @@ class SignIn extends React.Component {
 						required
 						handleOnChange={this.handleOnChange}
 					/>
-					<div className="buttons">
+					<ButtonsContainer>
 						<CustomButton type="submit"> Sign in </CustomButton>
 						<CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
 							{' '}
 							Sign in with Google{' '}
 						</CustomButton>
-					</div>
+					</ButtonsContainer>
 				</form>
-			</div>
+			</SignInContainer>
 		);
 	}
 }
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -4,7 +4,7 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
-import './sign-up.styles.scss';
+import { SignUpContainer, TitleContainer } from './sign-up.styles';
 
 class SignUp extends React.Component {
 	constructor() {
@@ -49,8 +49,8 @@ class SignUp extends React.Component {
 	render() {
 		const { displayName, email, password, confirmPassword } = this.state;
 		return (
-			<div className="sign-up">
-				<h2 className="title">You don't have an account ?</h2>
+			<SignUpContainer>
+				<TitleContainer>You don't have an account ?</TitleContainer>
 				<span>Sign up with your email and password</span>
 				<form onSubmit={this.handleSubmit}>
 					<FormInput
@@ -89,7 +89,7 @@ class SignUp extends React.Component {
 						<CustomButton type="submit"> SIGN UP </CustomButton>
 					</div>
 				</form>
-			</div>
+			</SignUpContainer>
 		);
 	}
 }
