7085770a001e79e9c115f235255e7fa1cfc4e2a7
miraDask <mira.daskalov@gmail.com>
2020-05-18 15:35:28 +0100

    replace css files with styled-components for directory, collections-overview and collection-preview components

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import CollectionItem from '../collection-item/collection-item.component';
 
-import './collection-preview.styles.scss';
+import { CollectionPreviewContainer, CollectionTitle, ItemPreviewContainer } from './collection-preview.styles';
 
 const CollectionPreview = ({ title, items }) => (
-	<div className="collection-preview">
-		<h1 className="title">{title.toUpperCase()}</h1>
-		<div className="preview">
+	<CollectionPreviewContainer>
+		<CollectionTitle>{title.toUpperCase()}</CollectionTitle>
+		<ItemPreviewContainer>
 			{items.filter((item, indx) => indx < 4).map((item) => <CollectionItem key={item.id} item={item} />)}
-		</div>
-	</div>
+		</ItemPreviewContainer>
+	</CollectionPreviewContainer>
 );
 
 export default CollectionPreview;
